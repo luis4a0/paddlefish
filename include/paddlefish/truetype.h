bc76c57b3b2ddec1a4e7c79b25c664d37c2b06d8
Luis Peñaranda <luis.penaranda@gmx.com>
2022-10-15 12:29:48 -0300

    Add STB TrueType library.
    All the functionality of the new library is compiled in truetype.cpp.
    For using it, it is now necessary to include paddlefish/truetype.h.

---
+++ include/paddlefish/truetype.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2022 Luis Peñaranda. All rights reserved.
+//
+// This file is part of paddlefish.
+//
+// Paddlefish is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Paddlefish is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with paddlefish.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef PADDLEFISH_TRUETYPE_H
+#define PADDLEFISH_TRUETYPE_H
+
+#ifdef PADDLEFISH_USE_STB_TRUETYPE
+#include <stb_truetype.h>
+#endif
+
+#endif // PADDLEFISH_TRUETYPE_H
+
+// vim: ts=2:sw=2:expandtab
